0e4fdb6f970ab733e98e543dd706fbb7d21f6563
Amit <amit08cse53@gmail.com>
2018-10-15 00:11:53 +0530

    commit

---
+++ app/src/main/java/Utility/AvenuesParams.java
@@ -0,0 +1,37 @@
+package Utility;
+
+
+public class AvenuesParams {
+    public static final String COMMAND = "command";
+    public static final String ACCESS_CODE = "access_code";
+    public static final String MERCHANT_ID = "merchant_id";
+    public static final String ORDER_ID = "order_id";
+    public static final String AMOUNT = "amount";
+    public static final String CURRENCY = "currency";
+    public static final String ENC_VAL = "enc_val";
+    public static final String REDIRECT_URL = "redirect_url";
+    public static final String CANCEL_URL = "cancel_url";
+    public static final String RSA_KEY_URL = "rsa_key_url";
+
+//    public static final String COMMAND = "command";
+    public static final String ACCESS_CODE_VALUE = "AVEJ80FJ83AH28JEHA";
+    public static final String MERCHANT_ID_VALUE = "192856";
+//    public static final String ORDER_ID_VALUE = "order_id";
+//    public static final String AMOUNT = "amount";
+//    public static final String CURRENCY = "currency";
+//    public static final String ENC_VAL = "enc_val";
+    public static final String REDIRECT_URL_VALUE = "http://43.231.127.184:4507/sou/ccavResponseHandler.php";
+    public static final String CANCEL_URL_VALUE = "http://43.231.127.184:4507/sou/ccavResponseHandler.php";
+    public static final String RSA_KEY_URL_VALUE = "http://43.231.127.184:4507/sou/GetRSS.php";
+//    public static final String RSA_KEY_URL = "https://secure.ccavenue.com/transaction/getRSAKey";
+
+//[10:18 PM, 10/14/2018] Kajal Chauhan, TM Systems: Redirect url :
+//    http://43.231.127.184:4507/sou/ccavResponseHandler.php
+//            [10:18 PM, 10/14/2018] Kajal Chauhan, TM Systems: RSA key url :
+//
+//    http://43.231.127.184:4507/sou/GetRSS.php
+//            [10:18 PM, 10/14/2018] Kajal Chauhan, TM Systems: GetRSA.php**
+//            [10:18 PM, 10/14/2018] Kajal Chauhan, TM Systems: Cancel url :
+//    http://43.231.127.184:4507/sou/ccavResponseHandler.php
+
+}
